0c771df7f116d41583d7ed81a8d7efee9b3f9675
postmodern <postmodern.mod3@gmail.com>
2008-06-29 11:39:06 +0000

    * Large update of old code.

--- lib/ronin/php/lfi.rb
+++ lib/ronin/php/lfi.rb
@@ -1,4 +1,5 @@
 #
+#--
 # Ronin PHP - A Ruby library for Ronin that provides support for PHP
 # related security tasks.
 #
@@ -17,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#++
 #
 
 require 'ronin/extensions/uri'
