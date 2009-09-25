8a79062920527318289ecb0a41df2a0b6ab3d53d
postmodern <postmodern.mod3@gmail.com>
2009-09-24 20:21:46 -0700

    Added some YARD tags to LFI::Target.

--- lib/ronin/php/lfi/target.rb
+++ lib/ronin/php/lfi/target.rb
@@ -34,7 +34,11 @@ module Ronin
         attr_reader :extractors
 
         #
-        # Creates a new Path object with the specified _path_ and _pattern_.
+        # Creates a new Target object.
+        #
+        # @yield [target]
+        #   If a block is given, it will be passed the newly created
+        #   Target object.
         #
         def initialize(&block)
           @paths = Hash.new { |hash,key| hash[key] = [] }
@@ -46,29 +50,49 @@ module Ronin
         end
 
         #
-        # Returns the supported OSes.
+        # The supported OSes.
+        #
+        # @return [Array]
+        #   OSes that the targeted file can be found on.
         #
         def oses
           @paths.keys
         end
 
         #
-        # Returns all the paths of the target.
+        # All the paths of the target.
+        #
+        # @return [Array]
+        #   Paths of the targetted file.
         #
         def all_paths
           @paths.values.flatten.uniq
         end
 
         #
-        # Returns the paths for the target commonly found on the specified
-        # _os_.
+        # Finds the paths for the targetted file commonly found on a given
+        # OS.
+        #
+        # @param [String] os
+        #   The OS to search for.
+        #
+        # @return [String]
+        #   The path that the targetted file can be found at on the given
+        #   OS.
         #
         def paths_for(os)
           @paths[os]
         end
 
         #
-        # Iterates over each path passing each one to the specified _block_.
+        # Iterates over each path.
+        #
+        # @yield [path]
+        #   A given block will be passed each known path of the targetted
+        #   file.
+        #
+        # @yieldparam [String] path
+        #   A path that the targetted file is known to reside at.
         #
         def each_path(&block)
           @paths.each_value do |os_paths|
@@ -77,8 +101,14 @@ module Ronin
         end
 
         #
-        # Returns +true+ if the specified _body_ has the path included in
-        # it, returns +false+ otherwise.
+        # Determines if the given body of text contains the targetted file.
+        #
+        # @param [String] body
+        #   The body of text to examine.
+        #
+        # @return [Boolean]
+        #   Specifies whether the a given body of text has the targetted
+        #   file included in it.
         #
         def included_in?(body)
           if @recognizor
@@ -89,15 +119,27 @@ module Ronin
         end
 
         #
-        # Add an extraction rule with the specified _name_ and the
-        # specified _pattern_.
+        # Add an extraction rule to the targetted file.
+        #
+        # @param [Symbol] name
+        #   The name of the rule.
+        #
+        # @param [Regexp] pattern
+        #   The pattern to extract.
         #
         def extract(name,pattern)
           @extractors[name] = pattern
         end
 
         #
-        # Extracts data from the specified _body_ of HTML.
+        # Extracts data from a given body of text.
+        #
+        # @param [String] body
+        #   The body of text to extract data from.
+        #
+        # @return [Hash]
+        #   The extracted data, grouped by the name of the extract rule
+        #   and the extracted data.
         #
         def extract_from(body)
           data = {}
@@ -120,36 +162,60 @@ module Ronin
         end
 
         #
-        # Returns all Target categories.
+        # All Target categories.
+        #
+        # @return [Hash]
+        #   All categories of targetted files.
         #
         def Target.categories
           @@categories ||= Hash.new { |hash,key| hash[key] = [] }
         end
 
         #
-        # Returns +true+ if there is a category with the specified _name_,
-        # returns +false+ otherwise.
+        # Determines if any targetted files have been defined in a given
+        # category.
+        #
+        # @param [Symbol] name
+        #   The category to search for.
+        #
+        # @return [Boolean]
+        #   Specifies whether there is a category with the given name.
         #
         def Target.has_category?(name)
           Target.categories.has_key?(name)
         end
 
         #
-        # Returns the targets within the category with the specified _name_.
+        # Finds the targets within the category with a given name.
+        #
+        # @param [Symbol] name
+        #   The category to search for.
+        #
+        # @return [Array<Target>]
+        #   The targetted files within the category.
         #
         def Target.category(name)
           Target.categories[name]
         end
 
         #
-        # Returns all targets.
+        # All targets.
+        #
+        # @return [Array<Target>]
+        #   All targetted files.
         #
         def Target.all
           Target.categories.values.flatten
         end
 
         #
-        # Iterates over all targets, passing each to the given _block_.
+        # Iterates over all targets.
+        #
+        # @yield [target]
+        #   The given block will be passed each registered targetted file.
+        #
+        # @yieldparam [Target] target
+        #   A targetted file.
         #
         def Target.each(&block)
           Target.categories.each_value do |targets|
@@ -181,28 +247,43 @@ module Ronin
         end
 
         #
-        # Returns the targeted files used in testing for LFI.
+        # The targeted files used in testing for LFI.
+        #
+        # @return [Array<Target>]
+        #   The targetted files used to identify LFI vulnerabilities.
         #
         def Target.tests
           Target.category(:test)
         end
 
         #
-        # Returns the targeted configuration files.
+        # The targeted configuration files.
+        #
+        # @return [Array<Target>]
+        #   The targetted configuration files.
         #
         def Target.configs
           Target.category(:config)
         end
 
         #
-        # Returns the targeted log files.
+        # The targeted log files.
+        #
+        # @return [Array<Target>]
+        #   The targetted log files.
         #
         def Target.logs
           Target.category(:log)
         end
 
         #
-        # Returns all targets for the specified _os_.
+        # All targets for a given OS.
+        #
+        # @param [String] os
+        #   The OS to find targetted files for.
+        #
+        # @return [Array<Target>]
+        #   All targetted files for the given OS.
         #
         def Target.targets_for(os)
           Target.each do |target|
@@ -213,7 +294,10 @@ module Ronin
         end
 
         #
-        # Returns all targets with extractors.
+        # All targets with extractors.
+        #
+        # @return [Array<Target>]
+        #   All targetted files with data extraction rules.
         #
         def Target.with_extractors
           targets = []
@@ -228,7 +312,13 @@ module Ronin
         end
 
         #
-        # Returns all targets with the specified file _name_.
+        # All targets with a matching file-name.
+        #
+        # @param [String] name
+        #   The file-name to search for.
+        #
+        # @return [Array<Target>]
+        #   The targetted files with the matching file-name.
         #
         def Target.with_file(name)
           Target.each do |target|
@@ -241,7 +331,13 @@ module Ronin
         protected
 
         #
-        # Defines a new Target in the specified category _name_.
+        # Defines a new targetted file in a given category.
+        #
+        # @param [Symbol] name
+        #   The category to define the targetted file within.
+        #
+        # @return [Target]
+        #   The newly defined targetted file.
         #
         def self.define(name,&block)
           new_target = Target.new(&block)
