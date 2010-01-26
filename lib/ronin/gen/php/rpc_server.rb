1a1eb6f7f40539bff45d4488893e2c1ae7afb82c
postmodern <postmodern.mod3@gmail.com>
2010-01-26 00:34:59 -0800

    Added a crude FileGenerator for generating the PHP-RPC Server.

---
+++ lib/ronin/gen/php/rpc_server.rb
@@ -0,0 +1,39 @@
+#
+# Ronin PHP - A Ruby library for Ronin that provides support for PHP
+# related security tasks.
+#
+# Copyright (c) 2007-2009 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'ronin/gen/php/config'
+require 'ronin/gen/file_generator'
+
+module Ronin
+  module Gen
+    module PHP
+      class RPCServer < FileGenerator
+
+        SERVER_FILE = File.join('ronin','gen','php','rpc','server.php.erb')
+
+        def generate
+          template SERVER_FILE, self.path
+        end
+
+      end
+    end
+  end
+end
