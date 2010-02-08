9ffca7cb08dd9f1abd63436a84888552e81c31c0
postmodern <postmodern.mod3@gmail.com>
2010-02-08 03:44:46 -0800

    Moved Ronin::Exploits::LFI and Ronin::Exploits::RFI into Ronin::Exploits::PHP.

--- lib/ronin/php/rfi.rb
+++ lib/ronin/php/rfi.rb
@@ -22,10 +22,3 @@
 require 'ronin/php/rfi/extensions'
 require 'ronin/php/rfi/rfi'
 require 'ronin/php/rfi/scanner'
-require 'ronin/database'
-
-module Ronin
-  Database.upgrade do
-    require 'ronin/exploits/rfi'
-  end
-end
