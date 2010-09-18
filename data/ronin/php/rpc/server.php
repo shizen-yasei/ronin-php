76950f1a03de8ab3072ab6b62594fab3fdbc3175
Postmodern <postmodern.mod3@gmail.com>
2010-09-18 01:39:21 -0700

    Put PHP // comments before <?php and ?>, in case our script is being required directly into PHP.

--- data/ronin/php/rpc/server.php
+++ data/ronin/php/rpc/server.php
@@ -1,25 +1,24 @@
-<!--
-<?php
-#
-# Ronin PHP-RPC Server - A PHP-RPC server designed to work in hostile
-# environments.
-#
-# Copyright (c) 2007-2009
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
+// <!-- <?php
+/*
+ * Ronin PHP-RPC Server - A PHP-RPC server designed to work in hostile
+ *  environments.
+ *
+ * Copyright (c) 2007-2009
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 /**
  * PHP MsgPack encode/decode
@@ -764,13 +763,7 @@ function fingerprint($params=array())
   return $profile;
 }
 
-#
-# Inlined PHP: Start
-#
 
-#
-# Inlined PHP: End
-#
 
 if (isset($_REQUEST['rpcrequest']))
 {
@@ -788,8 +781,6 @@ if (isset($_REQUEST['rpcrequest']))
   echo("<rpc-response>{$response}</rpc-response>");
   exit;
 }
-
-?>
--->
+// ?> -->
 
 
